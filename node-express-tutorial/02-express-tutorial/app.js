7b266c41e03b4894c1ed7176b1670835679af9c1
theanuragsoni <104461155+theanuragsoni@users.noreply.github.com>
2023-06-08 20:48:11 +0530

    navbar-app manual example

--- node-express-tutorial/02-express-tutorial/app.js
+++ node-express-tutorial/02-express-tutorial/app.js
@@ -1,1 +1,50 @@
-console.log('Express Tutorial')
+const http = require('http')
+const {readFileSync} = require('fs')
+
+const homepage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+
+    // Home Page
+    if (url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write(homepage)
+        res.end()
+
+        // About Page
+    } else if (url === '/about') {
+        res.writeHead(200, { 'content-type': 'text/plain' })
+        res.write('<h1>About Page</h1>')
+        res.end()
+
+        // CSS
+    } else if (url === '/styles.css') {
+        res.writeHead(200, { 'content-type': 'text/css' })
+        res.write(homeStyles)
+        res.end()
+
+        // image/logo
+    } else if (url === '/logo.svg') {
+        res.writeHead(200, { 'content-type': 'image/svg+xml' })
+        res.write(homeImage)
+        res.end()
+
+        // logic 
+    } else if (url === '/browser-app.js') {
+        res.writeHead(200, { 'content-type': 'text/javascript' })
+        res.write(homeLogic)
+        res.end()
+
+        // 404
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write('<h1>page not found</h1>')
+        res.end()
+    }
+})
+
+server.listen(5000)
