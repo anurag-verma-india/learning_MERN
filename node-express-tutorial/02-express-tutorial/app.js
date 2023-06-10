f49cf6baf2945141389cbc33a53d9c88de1fa60d
theanuragsoni <104461155+theanuragsoni@users.noreply.github.com>
2023-06-11 01:08:59 +0530

    06 11 00

--- node-express-tutorial/02-express-tutorial/app.js
+++ node-express-tutorial/02-express-tutorial/app.js
@@ -1,50 +1,26 @@
-const http = require('http')
-const {readFileSync} = require('fs')
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+const authorize = require('./authorize')
 
-const homepage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+// req => middleware => res
+app.use([logger, authorize])
 
-const server = http.createServer((req, res) => {
-    const url = req.url
-
-    // Home Page
-    if (url === '/') {
-        res.writeHead(200, { 'content-type': 'text/html' })
-        res.write(homepage)
-        res.end()
-
-        // About Page
-    } else if (url === '/about') {
-        res.writeHead(200, { 'content-type': 'text/plain' })
-        res.write('<h1>About Page</h1>')
-        res.end()
-
-        // CSS
-    } else if (url === '/styles.css') {
-        res.writeHead(200, { 'content-type': 'text/css' })
-        res.write(homeStyles)
-        res.end()
-
-        // image/logo
-    } else if (url === '/logo.svg') {
-        res.writeHead(200, { 'content-type': 'image/svg+xml' })
-        res.write(homeImage)
-        res.end()
-
-        // logic 
-    } else if (url === '/browser-app.js') {
-        res.writeHead(200, { 'content-type': 'text/javascript' })
-        res.write(homeLogic)
-        res.end()
-
-        // 404
-    } else {
-        res.writeHead(404, { 'content-type': 'text/html' })
-        res.write('<h1>page not found</h1>')
-        res.end()
-    }
+app.get('/', (req, res) => {
+    res.send('Home')
+})
+app.get('/api/products', (req, res) => {
+    res.send('Products')
+})
+app.get('/api/items', (req, res) => {
+    console.log(req.user)
+    res.send('items')
+})
+app.get('/about', (req, res) => {
+    res.send('About')
 })
 
-server.listen(5000)
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...')
+})
