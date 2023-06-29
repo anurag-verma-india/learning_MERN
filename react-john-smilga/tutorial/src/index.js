3e9b8ad929476b947e6c27408c8131a43a85a45f
theanuragsoni <104461155+theanuragsoni@users.noreply.github.com>
2023-06-30 04:46:51 +0530

    02:43:00

--- react-john-smilga/tutorial/src/index.js
+++ react-john-smilga/tutorial/src/index.js
@@ -2,26 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const author = 'Rebecca Yarros';
-const title = 'Iron Flame (The Empyrean, 2)';
-const img = './images/book-1.jpg';
+const books = [
+  {
+    author: 'Rebecca Yarros',
+    title: 'Iron Flame (The Empyrean, 2)',
+    img: './images/book-1.jpg',
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+  },
+];
+
+const names = ['john', 'peter', 'susan'];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
+      {names.map((name) => {
+        return <h1>{name}</h1>;
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
+  console.log(props);
+  const { title, author, img } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {console.log(props)}
     </article>
   );
 };
