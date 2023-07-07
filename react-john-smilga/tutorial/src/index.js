314407ea6c26b2cfefb155eb1dc103eef5b61803
theanuragsoni <104461155+theanuragsoni@users.noreply.github.com>
2023-07-08 03:28:21 +0530

    04:10:00

--- react-john-smilga/tutorial/src/index.js
+++ react-john-smilga/tutorial/src/index.js
@@ -1,42 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
-const books = [
-  {
-    author: 'Rebecca Yarros',
-    title: 'Iron Flame (The Empyrean, 2)',
-    img: './images/book-1.jpg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
-    id: 2,
-  },
-];
+import './index.css';
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book {...book} key={book.id} />;
-      })}
-    </section>
-  );
-};
-
-const Book = (props) => {
-  // console.log(props);
-  const { title, author, img } = props;
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>Amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          const { img, title, author } = book;
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
