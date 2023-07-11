51ff26c66c1470c2594519846454e9e0411f60b8
theanuragsoni <104461155+theanuragsoni@users.noreply.github.com>
2023-07-12 01:08:21 +0530

    05:00:00

--- react-john-smilga/my-react-app/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import viteLogo from '/vite.svg';
-import './App.css';
-
-function App() {
-  return <h1>our first vite project</h1>;
-}
-
-export default App;
---
+++ react-john-smilga/starting-about-4-hours/my-react-app/src/App.jsx
@@ -0,0 +1,8 @@
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return <h1>Hello world!</h1>;
+}
+
+export default App;
