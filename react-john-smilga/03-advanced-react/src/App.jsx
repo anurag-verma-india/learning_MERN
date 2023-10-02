af5339528e4ff4c52dbc6b91f645f1294811a000
theanuragsoni <104461155+theanuragsoni@users.noreply.github.com>
2023-10-03 02:34:32 +0530

    06:57:00 truthy and falsey

--- react-john-smilga/03-advanced-react/src/App.jsx
+++ react-john-smilga/03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
+import Starter from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview';
 import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data';
 
 function App() {
--- react-john-smilga/03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ react-john-smilga/03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState('Anurag');
+  // return <h2>The value is {first || second}</h2>;
+  return <h2>The value is {first && second}</h2>;
 };
 export default ShortCircuitOverview;
