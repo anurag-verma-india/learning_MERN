f94e9d2418a77adcc07f53a11f6500f907553da0
Anurag Verma <anuragsoniemail@gmail.com>
2023-12-09 22:44:19 +0530

    Started the FCC John Smilga react express tutorial

--- 01-node-tutorial/15-create-big-file.js
+++
@@ -1,5 +0,0 @@
-const { writeFileSync } = require('fs');
-
-for (i = 0; i < 100000; i++) {
-    writeFileSync('./content/big.txt', `Hello there ${i}\n`, {flag: 'a'})
-}
--- 01-node-tutorial/3-modules.js
+++
@@ -1,7 +0,0 @@
-const names = require('./4-name');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavor')
-require('./7-mind-grenade')
-sayHi('Susan')
-sayHi(names.john)
-sayHi(names.peter)
