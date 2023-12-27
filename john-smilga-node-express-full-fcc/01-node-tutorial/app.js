46f3ae77eb2a59f1b08cfd05402b8dbcd7193bbf
Anurag Verma <anuragsoniemail@gmail.com>
2023-12-27 17:37:57 +0530

    2023 12 27

--- john-smilga-node-express-full-fcc/01-node-tutorial/app.js
+++ john-smilga-node-express-full-fcc/01-node-tutorial/app.js
@@ -1,1 +1,9 @@
-console.log('Welcome to Node Tutorial')
+const obi = "Obi wan Kenobi"
+const vader = "Darth Vader"
+
+const sayHi = (name)=>{
+    console.log(`Hello There, ${name}`)
+}
+
+sayHi(obi)
+sayHi(vader)
