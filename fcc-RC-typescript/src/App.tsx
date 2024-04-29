3421b6f68e5d7257aa6d2401aa8730f3d3c348ec
Anurag Verma <104461155+anurag-verma-india@users.noreply.github.com>
2024-04-29 23:15:03 +0530

    Started learning typescript

---
+++ fcc-RC-typescript/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+let name: string;
+
+name = "Anurag";
+
+
+
+function App() {
+  return <div className="App">Hello World </div>;
+}
+
+export default App;
