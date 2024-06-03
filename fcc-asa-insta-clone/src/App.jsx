5cc912ad00eca640935e61c0f4a6713f0cdd4ac8
Anurag Verma <104461155+anurag-verma-india@users.noreply.github.com>
2024-06-03 15:13:20 +0530

    Insta Clone: Basic project file structure
    AuthPage, Sidebar structure, and basic project structure

--- fcc-asa-insta-clone/src/App.jsx
+++ fcc-asa-insta-clone/src/App.jsx
@@ -1,11 +1,16 @@
-import { Button } from "@chakra-ui/react";
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import PageLayout from "./Layouts/PageLayouts/PageLayout";
 function App() {
-  return (
-    <>
-      <Button>Hi Mom</Button>
-    </>
-  );
+    return (
+        <PageLayout>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/auth" element={<AuthPage />} />
+            </Routes>
+        </PageLayout>
+    );
 }
 
 export default App;
