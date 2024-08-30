432c9e13556622b75ba39c32f53febecda42c503
Anurag Verma <104461155+anurag-verma-india@users.noreply.github.com>
2024-08-30 23:32:04 +0530

    added portofolio site and a react project

---
+++ fcc-25-react/accordion/src/components/accordian/index.jsx
@@ -0,0 +1,11 @@
+// single selection
+
+import { useState } from "react";
+
+// multiple selection
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+
+    return <div>Accordian</div>;
+}
