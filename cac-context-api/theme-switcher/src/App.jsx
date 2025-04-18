da882663d36e27a2d67d1ab89c1fd0bf71ebb585
anurag-verma-india <104461155+anurag-verma-india@users.noreply.github.com>
2025-04-18 13:42:51 +0530

    ThemeSwitcher (context API) not working

--- cac-context-api/theme-switcher/src/App.jsx
+++ cac-context-api/theme-switcher/src/App.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
+
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
 
 function App() {
-    // const [count, setCount] = useState(0)
+    const [themeMode, setThemeMode] = useState("light");
+    // const [themeMode, setThemeMode] = useState("dark");
+
+    const lightTheme = () => {
+        setThemeMode("light");
+    };
+    const darkTheme = () => {
+        setThemeMode("dark");
+    };
+
+    useEffect(() => {
+        document.querySelector("html").classList.remove("light", "dark");
+        document.querySelector("html").classList.add(themeMode);
+    }, [themeMode]);
 
     return (
         <>
-            <div>Hello world</div>
+            <ThemeProvider value={(themeMode, darkTheme, lightTheme)}>
+                <div className="flex flex-wrap min-h-screen items-center">
+                    <div className="w-full">
+                        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                            <ThemeBtn />
+                        </div>
+
+                        <div className="w-full max-w-sm mx-auto">
+                            <Card />
+                        </div>
+                    </div>
+                </div>
+            </ThemeProvider>
         </>
     );
 }
--- cac-context-api/theme-switcher/tailwind.config.js
+++ cac-context-api/theme-switcher/tailwind.config.js
@@ -1,59 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-EventTargetodule.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: "class",
     theme: {
-        extend: {
-            colors: {
-                whitesmoke: {
-                    100: "#f9fafc",
-                    200: "#f2efef",
-                },
-                gray: {
-                    100: "#949494",
-                    200: "#8f8f8f",
-                    300: "#878787",
-                    400: "#808080",
-                    500: "#7d7d7d",
-                    600: "#777",
-                },
-                black: "#000",
-                lightgray: "#cecece",
-                gainsboro: {
-                    100: "#e3e3e3",
-                    200: "#dadada",
-                },
-                dimgray: {
-                    100: "#636363",
-                    200: "#505050",
-                },
-                white: "#fff",
-                silver: "#c7c7c7",
-                darkgray: {
-                    100: "#b4b4b4",
-                    200: "#b2b2b2",
-                },
-            },
-            fontFamily: {
-                inter: "Inter",
-                inherit: "inherit",
-            },
-            borderRadius: {
-                "81xl": "100px",
-            },
-            padding: {
-                "3xs": "10px",
-                xl: "20px",
-                "8xs": "5px",
-            },
-        },
-        fontSize: {
-            base: "16px",
-            "2xs": "11px",
-            xs: "12px",
-            inherit: "inherit",
-        },
+        extend: {},
     },
-    corePlugins: {
-        preflight: false,
-    },
+    plugins: [],
 };
